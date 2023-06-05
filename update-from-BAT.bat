f1ea9318fa08df2765932dccbbe41fcce534db16
ClioCJS <ClioCJS@gmail.com>
2023-06-04 22:43:36 -0400

    username change

--- update-from-BAT.bat
+++ update-from-BAT.bat
@@ -7,7 +7,8 @@ REM     local GIT repo beore doing anything significant.  Or really, before doin
 
 rem CONFIGURATION:
         SET MANIFEST_FILES=generate_and_play_random_midi.py generate_midi_randomly.py convert_midi_to_wav_with_soundfont.py play_wav_file.py generate-and-play-random-midi.btm randmidi.bat 
-        SET SECONDARY_BAT_FILES=
-        call update-from-BAT-via-manifest.bat
+        SET SECONDARY_BAT_FILES=clean-python-build-and-dist-folders.bat create-python-distribution-ZIP-for-compiled-EXE.bat update-from-BAT-via-manifest.bat compile-python-project-into-EXE.btm validate-env-var.bat validate-environment-variable.bat validate-environment-variables.bat update-from-BAT-via-manifest.bat
 
+
+call update-from-BAT-via-manifest.bat
 
